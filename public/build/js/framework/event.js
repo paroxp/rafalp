5841848edfcad5598e7a0ed7685b378ab192cd91
Rafał Proszowski <paroxp@gmail.com>
2015-08-01 15:22:37 +0100

    Adding trigger functionality.

--- public/build/js/framework/event.js
+++ public/build/js/framework/event.js
@@ -23,11 +23,46 @@ app.extend(app, {
      * Bind an event onto a specific action performed on an element, with a callback function.
      *
      * @param event
-     * @param callback
+     * @param first
+     * @param second
      */
-    on: function (event, callback) {
-        app.nodeAssure(this._wrapped, function (element) {
-            element.addEventListener(event, callback);
+    on: function (event, first, second) {
+        var func = typeof second !== 'undefined' ? second : first,
+            $element = typeof second !== 'undefined' ? $(first) : this;
+
+        app.nodeAssure($element._wrapped, function (element) {
+            element.addEventListener(event, func);
+        });
+
+        return this;
+    },
+
+    /**
+     * Trigger a specific event on specific element.
+     *
+     * @param event
+     * @param data
+     * @param element
+     */
+    trigger: function (event, data, element) {
+        data = typeof element !== 'undefined' ? data : {};
+
+        var $element = typeof element !== 'undefined' ? $(element) : this,
+            e;
+
+        e = document.createEvent('HTMLEvents');
+        e.initEvent(event, true, false);
+
+        // This may need to come in some time...
+        //if (window.CustomEvent) {
+        //    e = new CustomEvent(event, {detail: data});
+        //} else {
+        //    e = document.createEvent('CustomEvent');
+        //    e.initCustomEvent(event, true, true, data);
+        //}
+
+        app.nodeAssure($element._wrapped, function (element) {
+            element.dispatchEvent(e);
         });
 
         return this;
