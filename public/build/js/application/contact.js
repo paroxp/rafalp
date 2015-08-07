762b0973e72e36307b8e3a5307b2cd332364076c
Rafał Proszowski <paroxp@gmail.com>
2015-08-07 13:45:57 +0100

    Completing the logic behind contact form.

--- public/build/js/application/contact.js
+++ public/build/js/application/contact.js
@@ -18,21 +18,86 @@ _.extend(Application, {
         submit: function (e) {
             e.preventDefault();
 
-            var $form = this;
+            var $form = this,
+                json,
+                data = new FormData($form);
+
+            // Add an AJAX spinner.
+            $form
+                .parent()
+                .find('h2 span')
+                .html('<i class="fa fa-spinner fa-spin"></i>');
 
             // Disable submit button for the sake of greatness.
             $form
                 .find('button[type=submit]')
-                .attr('disable', 'disable');
+                .attr('disabled', 'disabled');
+
+            // Remove any previous messages.
+            $form
+                .parent()
+                .find('p.message, [data-error]')
+                .remove();
 
             app.request({
                 url: $form.attr('action'),
                 method: $form.attr('method'),
+                data: data,
                 success: function (request) {
-                    Application.contact.handleMessage($form, request.response.message, 'success');
+                    // Remove AJAX spinner.
+                    $form
+                        .parent()
+                        .find('h2 span')
+                        .html('');
+
+
+                    json = JSON.parse(request.responseText);
+
+                    if (request.status === 200) {
+                        Application.contact.handleMessage($form, json.message, 'success');
+
+                        // Add a check.
+                        $form
+                            .parent()
+                            .find('h2 span')
+                            .html('<i class="fa fa-check green"></i>');
+
+                    } else if (request.status === 400) {
+                        _.each(json.data, function (value, key) {
+                            app.validator.addError(value[0], key, $form.attr('data-validation'));
+                        });
+
+                        $form
+                            .trigger('validation-error');
+
+                        $form
+                            .find('button[type=submit]')
+                            .unset('disabled');
+
+                        Application.contact.handleMessage($form, json.message, 'error');
+                    } else {
+                        $form
+                            .find('button[type=submit]')
+                            .unset('disabled');
+
+                        Application.contact.handleMessage($form, json.message, 'error');
+                    }
                 },
                 error: function (request) {
-                    Application.contact.handleMessage($form, request.response.message, 'error');
+                    // Remove AJAX spinner.
+                    $form
+                        .parent()
+                        .find('h2 span')
+                        .html('');
+
+                    $form
+                        .find('button[type=submit]')
+                        .unset('disabled');
+
+                    json = JSON.parse(request.responseText);
+
+                    Application.contact
+                        .handleMessage($form, 'We couldn\'t send your message... Please try again later.', 'error');
                 }
             });
 
@@ -44,6 +109,7 @@ _.extend(Application, {
          *
          * @param $form
          * @param message
+         * @param type
          */
         handleMessage: function ($form, message, type) {
             $form
