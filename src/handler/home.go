b2c67773559ab16b832c467ace59099c374a150c
Rafał Proszowski <paroxp@gmail.com>
2015-08-23 09:41:59 +0100

    Preparation for the views system.

--- src/handler/home.go
+++ src/handler/home.go
@@ -1,15 +1,11 @@
 package handler
 
-import (
-	"fmt"
-	// "github.com/gorilla/mux"
-	"net/http"
-)
+import "net/http"
 
 // Home defines the default handler.
 type Home struct{}
 
 // Index prints out the default route for the web application.
 func (h Home) Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome!")
+
 }
