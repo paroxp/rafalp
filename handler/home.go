745d0970b7e4cd8a59c19ee6844782ba035bc0ac
Rafał Proszowski <paroxp@gmail.com>
2015-08-23 10:49:43 +0100

    Moving the source into the main content.

--- handler/home.go
+++ handler/home.go
@@ -1,11 +1,17 @@
 package handler
 
-import "net/http"
+import (
+	"net/http"
+	"text/template"
+)
 
 // Home defines the default handler.
 type Home struct{}
 
 // Index prints out the default route for the web application.
 func (h Home) Index(w http.ResponseWriter, r *http.Request) {
+	t := template.New("Home.Index")
+	t, _ = template.ParseFiles("./view/layout/master.html", "./view/home/index.html")
 
+	t.Execute(w, nil)
 }
