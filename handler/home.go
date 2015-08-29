c52c7845156b47d3fac96f7ff63560b32721537d
Rafał Proszowski <paroxp@gmail.com>
2015-08-29 12:23:27 +0100

    Removing some testing func.

--- handler/home.go
+++ handler/home.go
@@ -1,11 +1,8 @@
 package handler
 
 import (
-	"log"
 	"net/http"
 	"text/template"
-
-	"github.com/gorilla/context"
 )
 
 // Home defines the default handler.
@@ -13,7 +10,6 @@ type Home struct{}
 
 // Index prints out the default route for the web application.
 func (h Home) Index(w http.ResponseWriter, r *http.Request) {
-	log.Print(context.Get(r, "config"))
 	t := template.New("Home.Index")
 	t, _ = template.ParseFiles("./view/layout/master.html", "./view/home/index.html")
 
