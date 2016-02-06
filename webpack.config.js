c71040f918dd31f76e733db4bc1b856c272c0dfd
Rafał Proszowski <paroxp@gmail.com>
2016-02-06 14:03:02 +0000

    Add css and fonts to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,26 @@ module.exports = {
                 query: {
                     presets: ['react', 'es2015']
                 }
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style', 'css', 'sass']
+            },
+            {
+                test: /\.(woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&minetype=application/font-woff'
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader'
             }
+        ]
+    },
+    sassLoader: {
+        includePaths: [
+            './node_modules/bourbon/app/assets/stylesheets',
+            './node_modules/font-awesome/scss',
+            './node_modules/susy/sass'
         ]
     }
 };
