aa62802709d208df3e2a0fc643018434b77b0cac
Rafał Proszowski <paroxp@gmail.com>
2016-02-06 20:33:09 +0000

    Implement basic not found page

--- app/component/layout/NotFound.jsx
+++ app/component/layout/NotFound.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import Header from '../layout/Header.jsx';
 
 class NotFound extends React.Component {
     constructor() {
         super();
-
-        Header().setCurrent('404');
     }
 
     render() {
         return (
-            <main role="main">
+            <main role="main" className="not-found">
                 <h1>Oops!</h1>
                 <p>Looks like you're lost...</p>
             </main>
