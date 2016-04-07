49c6b8cca058508072f909c79d2c6a7ecb365146
Rafal Proszowski <rafal@flipsports.com>
2016-04-07 16:59:22 +0100

    Minify the JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var webpack = require('webpack');
 
 module.exports = {
+    // devtool: 'cheap-module-source-map',
     entry: {
         initial: [
             './scss/initial.scss'
@@ -57,6 +59,11 @@ module.exports = {
         new ExtractTextPlugin('../css/[name].css', {
             allChunks: true,
             disable: false
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
         })
     ],
     sassLoader: {
