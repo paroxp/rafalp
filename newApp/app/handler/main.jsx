c53072e5d60542e62690e9bb3a52eff82359505b
Rafal Proszowski <rafal@flipsports.com>
2016-04-20 15:58:46 +0100

    Add basic handlers

---
+++ newApp/app/handler/main.jsx
@@ -0,0 +1,13 @@
+import Base from './base.jsx';
+
+class Main extends Base {
+    index() {
+        console.log('Hello World!');
+    }
+
+    test() {
+        console.log('Testing...');
+    }
+}
+
+export default Main;
