347f733b0912b1d4eebf633a5175b51286202b87
Rafal Proszowski <rafal@flipsports.com>
2016-04-20 15:57:48 +0100

    Add simple webpack instance

---
+++ newApp/webpack.config.js
@@ -0,0 +1,37 @@
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        main: [
+            './app/main.jsx'
+        ]
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    output: {
+        filename: '[name].js',
+        path: './js'
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            server: {
+                baseDir: [
+                    './'
+                ]
+            }
+        })
+    ]
+};
