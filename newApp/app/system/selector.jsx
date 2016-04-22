9db7f7b8ad7d7cfcd58a8b9053dac1afa9308a6f
Rafal Proszowski <rafal@flipsports.com>
2016-04-22 11:02:13 +0100

    Extend the selector functionality

--- newApp/app/system/selector.jsx
+++ newApp/app/system/selector.jsx
@@ -1,5 +1,43 @@
 class Selector {
     /**
+     * Add specific class to the dom element.
+     *
+     * @param className
+     */
+    addClass(className) {
+        if (!this.elements.length) {
+            return;
+        }
+
+        for (let element of this.elements) {
+            let classes = element.className.split(' ');
+
+            classes.push(className);
+
+            element.className = classes.join(' ');
+        }
+
+        return this;
+    }
+
+    /**
+     * Add specific syntax after the selected elements.
+     *
+     * @param syntax
+     */
+    after(syntax) {
+        if (!this.elements.length) {
+            return;
+        }
+
+        for (let element of this.elements) {
+            element.insertAdjacentHTML('afterend', syntax);
+        }
+
+        return this;
+    }
+
+    /**
      * Find any element matching the handle.
      *
      * @param handle
@@ -23,6 +61,47 @@ class Selector {
         for (let element of this.elements) {
             element.addEventListener(event, callback);
         }
+
+        return this;
+    }
+
+    /**
+     * Ability to remove the element.
+     */
+    remove() {
+        if (!this.elements.length) {
+            return;
+        }
+
+        for (let element of this.elements) {
+            element.parentNode.removeChild(element);
+        }
+
+        return this;
+    }
+
+    /**
+     * Ability to remove a class from the dom element.
+     *
+     * @param className
+     */
+    removeClass(className) {
+        if (!this.elements.length) {
+            return;
+        }
+
+        for (let element of this.elements) {
+            let classes = element.className.split(' '),
+                index = classes.indexOf(className);
+
+            if (index >= 0) {
+                classes.splice(index, 1);
+
+                element.className = classes.join(' ');
+            }
+        }
+
+        return this;
     }
 }
 
