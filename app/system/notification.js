9f15ba6f263072c347019c2b6617136fb03fa11d
Rafał Proszowski <paroxp@gmail.com>
2016-07-20 23:08:38 +0100

    Fix and enhance the notification module

--- app/system/notification.js
+++ app/system/notification.js
@@ -1,5 +1,7 @@
+import * as common from './common';
 import notification from '../config/notification';
 import Security from './security';
+import $ from './selector';
 
 class Notification {
     /**
@@ -13,10 +15,46 @@ class Notification {
     constructor(type, content, title = null, expiry = null) {
         this.id = Security.guid();
         this.content = content;
-        this.className = notification.className + ' ' + notification[type].className;
+        this.className = notification.className;
         this.expiry = expiry || notification.expiry;
         this.title = title || notification.title;
         this.type = type;
+        this.typeClass = notification[type].className;
+
+        this.desolve = null;
+        this.element = null;
+
+        this.fire();
+    }
+
+    /**
+     * Fire the destroy event.
+     */
+    destroy() {
+        let notification = this;
+
+        if (notification.desolve !== null) {
+            clearTimeout(notification.desolve);
+        }
+
+        return $(notification.element).trigger('notification::destroy');
+    }
+
+    /**
+     * Fire the notification.
+     */
+    fire() {
+        let holder = document.createElement('div');
+        holder.innerHTML = this.getSyntax();
+
+        $(notification.container).append(holder.firstChild);
+        this.element = document.querySelector(notification.container).lastChild;
+
+        window.app.notification[this.id] = this;
+
+        this.setDesolver();
+
+        $(this.element).trigger('notification::fire');
     }
 
     /**
@@ -25,11 +63,28 @@ class Notification {
      * @returns string
      */
     getSyntax() {
+        let template = common.clone(notification.template);
+
         for (let i = 0, keys = Object.keys(this); i < keys.length; i++) {
-            notification.template.replace(/{keys[i]}/g, this[keys[i]]);
+            template = template.replace(new RegExp('{' + keys[i] + '}', 'g'), this[keys[i]]);
         }
 
-        return notification.template;
+        return template;
+    }
+
+    /**
+     * Set the desolver to to destroy notification after the expiry date.
+     */
+    setDesolver() {
+        let notification = this;
+
+        if (notification.desolve !== null) {
+            clearTimeout(notification.desolve);
+        }
+
+        notification.desolve = setTimeout(() => notification.destroy(), this.expiry * 1000);
+
+        $(this.element).trigger('notification::reset');
     }
 }
 
