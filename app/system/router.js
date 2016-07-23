78259954c6c52f4f691f054e0f37820628399f20
Rafał Proszowski <paroxp@gmail.com>
2016-07-23 19:10:17 +0100

    Correct the router handling

--- app/system/router.js
+++ app/system/router.js
@@ -1,17 +1,7 @@
+import Aviator from 'aviator';
 import routes from '../config/routes';
 
 class Router {
-    /**
-     * Parse the hash path to be used throughout the system.
-     *
-     * @returns {string}
-     */
-    static getPath() {
-        var hash = window.location.hash === '' ? '#/' : window.location.hash;
-
-        return hash.substr(1);
-    }
-
     /**
      * Change the hash url on the fly.
      *
@@ -19,33 +9,7 @@ class Router {
      * @returns {*}
      */
     static goTo(path) {
-        if (path === Router.getPath()) {
-            return false;
-        }
-
-        window.location.hash = '#' + path;
-
-        let router = new Router;
-
-        return router.run();
-    }
-
-    /**
-     * Attempt to throw a notFound response.
-     *
-     * @returns {*}
-     */
-    notFound() {
-        let action, handler;
-
-        try {
-            handler = new routes.notFound.handler;
-            action = handler[routes.notFound.action];
-        } catch ($e) {
-            console.error('Unable to obtain notFound handler.');
-        }
-
-        return action();
+        return Aviator.navigate(path);
     }
 
     /**
@@ -54,28 +18,9 @@ class Router {
      * @returns {*}
      */
     run() {
-        let action, handler,
-            Route = routes[Router.getPath()];
+        Aviator.setRoutes(routes);
 
-        if (Route === undefined) {
-            return this.notFound();
-        }
-
-        try {
-            handler = new Route.handler;
-        } catch (e) {
-            console.error('Could not find the "' + Route.handler + '" controller.');
-            return this.notFound();
-        }
-
-        try {
-            action = handler[Route.action];
-        } catch (e) {
-            console.error('The "' + Route.handler + '::' + Route.action + '" method is not defined.');
-            return this.notFound();
-        }
-
-        return action();
+        return Aviator.dispatch();
     }
 }
 
