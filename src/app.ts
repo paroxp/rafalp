d390d5a6cda69506f79c94794205803bb0630d4d
Rafal Proszowski <paroxp@gmail.com>
2020-09-11 19:35:15 +0100

    Move render function to layout middleware

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,14 @@
 import Router from '@koa/router';
-import Koa, { Middleware } from 'koa';
+import Koa from 'koa';
 import serve from 'koa-static';
-import { ReactElement } from 'react';
-import { renderToString } from 'react-dom/server';
 
 import { About } from './components/about';
 import { Home } from './components/home';
-import { htmlDocument } from './components/layout';
-import { config, Config } from './config';
+import { render } from './components/layout';
+import { config } from './config';
 
 const app = new Koa();
 const router = new Router();
-
-function render(cfg: Config, page: () => ReactElement, overrides?: object): Middleware {
-  const content = page();
-
-  return async (ctx: Koa.Context, next: Koa.Next): Promise<void> => {
-    ctx.body = htmlDocument({ ...cfg, ...overrides }, renderToString(content));
-    await next();
-  };
-}
 
 router.get('home', '/', render(config, Home));
 router.get('about', '/about', render(config, About, { subtitle: 'Resume' }));
---
+++ src/components/layout/middleware.ts
@@ -0,0 +1,17 @@
+
+import { Context, Middleware, Next } from 'koa';
+import { ReactElement } from 'react';
+import { renderToString } from 'react-dom/server';
+
+import { Config } from '../../config';
+
+import { htmlDocument } from './layout';
+
+export function render(cfg: Config, page: () => ReactElement, overrides?: object): Middleware {
+  const content = page();
+
+  return async (ctx: Context, next: Next): Promise<void> => {
+    ctx.body = htmlDocument({ ...cfg, ...overrides }, renderToString(content));
+    await next();
+  };
+}
