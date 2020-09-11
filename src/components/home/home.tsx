76aa08d8c78359e0c6e2d8c6562d95a2d136e594
Rafal Proszowski <paroxp@gmail.com>
2020-09-11 20:11:59 +0100

    Move out of London

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -2,7 +2,7 @@ import fs from 'fs';
 
 import React, { ReactElement } from 'react';
 
-import { Footer, Header } from '../layout';
+import { Footer, Header, Link } from '../layout';
 
 export function Home(): ReactElement {
   const beard = fs.readFileSync(`${__dirname}/../../../dist/img/beard.svg`, 'utf8');
@@ -13,8 +13,8 @@ export function Home(): ReactElement {
       <div dangerouslySetInnerHTML={{ __html: beard }}></div>
 
       <p>
-        Howdy! My name is Rafal and I am a friendly and hairy developer, based in London, UK. Currently, I'm serving
-        my duty at <a href="https://gds.blog.gov.uk/">Government Digital Service</a>. I am also trying to keep up
+        Howdy! My name is Rafal and I am a friendly and hairy developer, based in Newcastle, UK. Currently, I'm serving
+        my duty at <Link href="https://gds.blog.gov.uk/">Government Digital Service</Link>. I am also trying to keep up
         with the cool stuff by attending amazing local meet ups!
       </p>
     </main>
