84a1f4d06b1be4233823526cecfc078803fe38fe
Rafal Proszowski <paroxp@gmail.com>
2023-02-15 22:45:11 +0000

    devenv: initialise nix environment
    Nix allows me to declaratively set all the needed dependencies and
    software to work on this project.

---
+++ devenv.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+
+{
+  env.GREET = "rafalp.com";
+
+  packages = [ pkgs.git ];
+
+  languages.javascript.enable = true;
+  languages.typescript.enable = true;
+
+  enterShell = ''
+    git --version
+    echo "Node $(node --version)"
+    echo "NPM $(npm --version)"
+  '';
+}
