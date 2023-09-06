d0e3646bffc7fd49f6924ea27ac0dc12cee45a8b
Rafal Proszowski <paroxp@gmail.com>
2023-09-06 15:57:58 +0100

    generator: improve

--- src/generator.ts
+++ src/generator.ts
@@ -1,5 +1,7 @@
 import { writeFileSync } from 'fs';
 
+import { ReactElement } from 'react';
+
 import { config, distDir } from './config';
 import { About } from './pages/about';
 import { NotFound } from './pages/errors';
@@ -7,22 +9,45 @@ import { Home } from './pages/home';
 import { generateSiteMap } from './pages/sitemap';
 import { render } from './utils';
 
+interface Page {
+  readonly body: string;
+  readonly extension?: string;
+  readonly filename?: string;
+  readonly name: string;
+  readonly path: string;
+}
+
+function fromReact(body: () => ReactElement, path: string): string {
+  return render(config, body, { path });
+}
+
+function iterateFiles(filenames: readonly string[], page: Page): readonly string[] {
+  const filename = page.filename || `${page.name}${page.extension || '.html'}`;
+
+  writeFileSync(distDir(filename), page.body);
+
+  return [...filenames, filename];
+}
+
 async function generator(): Promise<void> {
-  const pages = [
-    { body: Home, file: 'index.html', name: 'home', path: '/' },
-    { body: NotFound, file: '404.html', name: '404', path: '/404' },
-    { body: About, file: 'about.html', name: 'about', path: '/about' },
+  const pages: readonly Page[] = [
+    { body: fromReact(Home, '/'), filename: 'index.html', name: 'home', path: '/' },
+    { body: fromReact(NotFound, '/404'), name: '404', path: '/404' },
+    { body: fromReact(About, '/about'), name: 'about', path: '/about' },
   ];
-  console.info(`${pages.length} pages to iterate.`);
 
-  pages.map(({ body, file, path }) => {
-    writeFileSync(distDir(file), render(config, body, { path }));
-    console.info(`'${file}' page generated.`);
-  });
+  const sitemap = {
+    body: await generateSiteMap(config, pages),
+    filename: 'sitemap.xml',
+    name: 'sitemap',
+    path: '/sitemap.xml',
+  };
 
-  const sitemap = await generateSiteMap(config, pages);
-  writeFileSync(distDir('sitemap.xml'), sitemap);
-  console.info('sitemap.xml file generated.');
+  const queue = [ ...pages, sitemap ];
+  console.info(`${queue.length} pages to iterate.`);
+
+  const filenames = queue.reduce(iterateFiles, []);
+  filenames.map(filename => console.info(`'${filename}' page generated.`));
 }
 
 generator()
