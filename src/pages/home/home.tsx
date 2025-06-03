8b193c29e16a7f9e0dfdcaa1ea61723430d894b5
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2025-06-03 14:25:29 +0100

    home: update location

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -10,7 +10,7 @@ export function Home(): ReactElement {
       <div dangerouslySetInnerHTML={{ __html: beard }}></div>
 
       <p>
-        Howdy! My name is Rafal and I am a friendly and hairy developer, based in Newcastle, UK. Currently, I'm serving
+        Howdy! My name is Rafal and I am a friendly and hairy developer, based in London, UK. Currently, I'm serving
         my duty at <Link href="https://gds.blog.gov.uk/">Government Digital Service</Link>. I am also trying to keep up
         with the cool stuff by attending amazing local meet ups!
       </p>
